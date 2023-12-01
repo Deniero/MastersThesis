39249637400903c3ad4ff70d84ed120da905643e
Daniel Sumann <31181392+Deniero@users.noreply.github.com>
2023-12-01 17:15:21 +0100

    added much better simulation duration. better text for foundation and implementations

--- simulations/evaluation/eval.R
+++ simulations/evaluation/eval.R
@@ -23,7 +23,7 @@ parse_csv_min_data <- function(raw_csv_results) {
 
 
 #### settings ####
-simulation_name = "sim_1"
+simulation_name = "sim_3"
 num_of_gen = 30
 res_row_size = 1
 res_column_size = 45
@@ -38,9 +38,20 @@ sim.def_diversity = read.csv(paste0('evaluation/results/', simulation_name, '_de
 
 #### merge data
 # merge best results with random results
-threshold <- quantile(sim.rand$results, 0.2)
-sim.rand_best <- subset(sim.rand, sim.rand$results <= threshold)
-sim.rand_t <- data.frame(t(sim.rand_best))
+#threshold <- quantile(sim.rand$results, 0.2)
+#sim.rand_best <- subset(sim.rand, sim.rand$results <= threshold)
+sim.rand_t <- data.frame(t(sim.rand))
+
+
+rand_fitness <- sim.rand
+rand_fitness$name <- rep(0:9, each = 96 * 31)
+rand_fitness$name <- as.factor(rand_fitness$name)
+rand_top_results <- rand_fitness %>%
+  group_by(name) %>%
+  summarize(max_result = min(results))
+sim.rand_t <- data.frame(t(rand_top_results$max_result))
+colnames(sim.rand_t) <- res_col_names
+
 sim.res <- rbind(parse_csv_min_data(sim.opt_min), parse_csv_min_data(sim.def_min))
 sim.res <- bind_rows(sim.res, sim.rand_t)
 sim.res$simulation <- c("Optimized", "Default", "Random")
@@ -74,14 +85,26 @@ sim.all_ga.melted$value <- ((3500 -sim.all_ga.melted$value) / 100)
 
 ##### plot data ####
 # plot performance comparison
-desired_levels <- c("Optimized", "Default")
 
-plot.bp <- ggplot(sim.res.melted, aes(simulation, value)) + 
-  geom_boxplot(data = subset(sim.res.melted, simulation %in% desired_levels), color="#457b9d") + 
-  geom_point(color="#457b9d")  +
-  labs(x = "", y = "Cummulated Emergency Break Duration (s)")
+plot.bp <- ggplot(sim.res.melted, aes(value, simulation)) + 
+  geom_boxplot(color="#457b9d") + 
+  labs(x = "Cummulated Emergency Break Duration", y = "")
 print(plot.bp)
-ggsave(paste0('evaluation/plots/', simulation_name, '_comparison.jpg'), plot = plot.bp, width = 12, height = 6, units = "cm", dpi = 600)
+ggsave(paste0('evaluation/plots/', simulation_name, '_comparison.jpg'), plot = plot.bp, width = 16, height = 6, units = "cm", dpi = 1000)
+
+#plot.bp.left <- ggplot(sim.res.melted, aes(value, simulation)) + 
+#  geom_boxplot(color="#457b9d") + coord_cartesian(xlim = c(-200, 0)) + scale_x_continuous(expand = c(0, 0)) + 
+#  labs(y = "", x = "")
+#print(plot.bp.left)
+
+#plot.bp.right <- ggplot(sim.res.melted, aes(value, simulation)) + 
+#  geom_boxplot(color="#457b9d") + coord_cartesian(xlim = c(0, 12)) + scale_x_continuous(expand = c(0, 0)) + 
+#  labs(y = "", x = "") + theme(axis.text.y = element_blank(),
+#                               axis.title.y = element_blank())
+#print(plot.bp.right)
+#ggsave(paste0('evaluation/plots/', simulation_name, '_comparison_left.jpg'), plot = plot.bp.left, width = 12, height = 6, units = "cm", dpi = 600)
+#ggsave(paste0('evaluation/plots/', simulation_name, '_comparison_right.jpg'), plot = plot.bp.right, width = 12, height = 6, units = "cm", dpi = 600)
+
 
 
 # plot diversity comparison
@@ -103,9 +126,10 @@ print(plot.generation_comparison)
 ggsave(paste0('evaluation/plots/', simulation_name, '_ga_generations.jpg'), plot = plot.generation_comparison, width = 12, height = 8, units = "cm", dpi = 600)
 
 
-##### t - test and effect size comparing default with optimized #####
+##### t - test and effect size #####
+print("comparing Optimized with Default")
 sim.res.melted.only_ga <- subset(sim.res.melted, simulation != "Random")
-sim.res.melted.only_ga <- subset(sim.res.melted.only_ga, !is.na(value))
+#sim.res.melted.only_ga <- subset(sim.res.melted.only_ga, !is.na(value))
 
 t_test<-t.test(value ~ simulation, data = sim.res.melted.only_ga)
 print(t_test)
@@ -116,29 +140,48 @@ r <- sqrt(t^2/(t^2+df))
 print("Effect Size:")
 print(round(r, 3))
 
+print("comparing Optimized with Random")
+sim.res.melted.rand_with_opt <- subset(sim.res.melted, simulation != "Default")
+#sim.res.melted.only_ga <- subset(sim.res.melted.only_ga, !is.na(value))
+
+t_test<-t.test(value ~ simulation, data = sim.res.melted.rand_with_opt)
+print(t_test)
+t<-t_test$statistic[[1]]
+df<-t_test$parameter[[1]]
+r <- sqrt(t^2/(t^2+df))
+
+print("Effect Size:")
+print(round(r, 3))
+
+
 
 #### plot random distribution ####
-rand_with_removed_value <- sim.rand
-rand_with_removed_value$results <- ((-rand_with_removed_value$results + 3500) / 100)
 
-test <- ggplot(rand_with_removed_value, aes(x = results)) +
-  geom_histogram(binwidth = 1, fill = "lightblue", color = "black", alpha = 0.7) +
-  labs(title = "Histogram", x = "Values", y = "Frequency")
+#rand_fitness$name_index <- rep(0:(nrow(rand_fitness) / (96 * 31) - 1), each = 96 * 31)
 
-print(test)
+#rand_fitness$name_index <- ave(rep(1, nrow(rand_fitness)), rand_fitness$name, FUN = seq_along) - 1
+#test <- ggplot(rand_fitness, aes(x = results)) +
+#  geom_histogram(binwidth = 1, fill = "lightblue", color = "black", alpha = 0.7) +
+#  labs(title = "Histogram", x = "Values", y = "Frequency")
+
+#print(test)
 
 
+#plot.rand_to_mean <- ggplot(rand_fitness, aes(x = name_index, y = results)) +
+#  geom_point() +
+#  facet_wrap(~name, scales = "free_x", ncol=10) +
+#  geom_hline(yintercept = 8.52, color = "red") +
+#  coord_cartesian(ylim = c(-180, 15)) +
+#  labs(x = "",
+#       y = "Cummulated Emergency Break Duration")
+#print(plot.rand_to_mean)
+
+#ggsave(paste0('evaluation/plots/', simulation_name, '_rand_to_mean.jpg'), plot = plot.rand_to_mean, width = 20, height = 8, units = "cm", dpi = 600)
 
 
-install.packages("fitdistrplus")
-library(fitdistrplus)
+#rand_top_results <- rand_fitness %>%
+#  group_by(name) %>%
+#  summarize(max_result = max(results))
 
-# Assuming skewed_data is your vector of skewed data
-fit <- fitdist(rand_with_removed_value$results, "lnorm")  # Fit a log-normal distribution
-
-# Summary of the fitted distribution
-summary(fit)
-
-# Plot the histogram and fitted distribution
-plot(fit)
-
+# Print the new dataframe
+#print(rand_top_results)
